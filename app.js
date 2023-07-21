4708887386d7893d9433b796967d4f8150518a04
AsmiVats <128238183+AsmiVats@users.noreply.github.com>
2023-07-21 12:16:05 +0530

    Update  app.js
    add mongoose database

--- app.js
+++ app.js
@@ -1,27 +1,75 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname+"/date.js");
 
 
 const app = express();
-
-let items =[];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/toDoDB');
+}
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+const Item = mongoose.model('Item',itemsSchema);
+
+const item1 = new Item({
+    name: "Enter your To-Do Task"
+});
+
+const defaultItems = [item1];
+
+app.get("/", function (req, res) {
     let day = date();
-    res.render("list",{kindOfDay: day,newListitems: items});
-})
+  
+    Item.find({})
+    .then(foundItems => {
+
+      if(foundItems.length == 0){
+        Item.insertMany(defaultItems);
+        res.redirect("/");
+      }
+      else{
+        res.render("list", { kindOfDay: day, newListItems: foundItems });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
 
 app.post("/",function(req,res){
-   let item = req.body.newitem;
-    items.push(item);
+   let itemName = req.body.newitem;
+   
+   const item = new Item({
+    name:itemName
+   })
+    item.save();
+
     res.redirect("/");
     console.log(item);
+})
+
+app.post("/delete",function(req,res){
+  const checked = req.body.checkbox;
+
+  Item.findOneAndDelete({_id: checked})
+  .then( () => {
+    console.log("Item deleted Successfully.")
+    res.redirect("/");
+  })
+  .catch(err =>{
+    console.log("err");
+  });
+
 })
 
 
